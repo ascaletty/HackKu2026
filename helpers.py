e14d48c36c30580d8c130a1e20e66472269af192
Aaron <ascaletty@ku.edu>
2026-04-18 15:43:26 -0500

    workin

--- helpers.py
+++ helpers.py
@@ -39,10 +39,41 @@ def autocomplete(addy):
         )
 
 
-autocomp = input("Input your addy")
-# print(autocomplete(autocomp))
-# #
-print(forwardGeocode(autocomp))
+def calc_route(source, destination):
+
+    headers = CaseInsensitiveDict()
+    headers["Accept"] = "application/json"
+    print(source)
+    print(destination)
+    # url=https://api.geoapify.com/v1/routing?waypoints=50.67902320667227,4.569876996843732%7C50.66170571489684,4.578667041603012&mode=drive&apiKey=d548c5ed24604be6a9dd0d989631f783
+    url = (
+        "https://api.geoapify.com/v1/routing?waypoints="
+        + str(source[1])
+        + ","
+        + str(source[0])
+        + "|"
+        + str(destination[1])
+        + ","
+        + str(destination[0])
+        + "&mode=drive&apiKey="
+        + config["API_KEY"]
+    )
+    print(url)
+    resp = requests.get(url, headers=headers)
+    if resp.status_code == 200:
+        print(resp.status_code)
+        resp_json = resp.json()
+        return resp_json
+    else:
+        raise ValueError(
+            f"server returned exit code {resp.status_code} with {resp.json()}"
+        )
+
+
+# autocomp = input("Input your addy")
+# # print(autocomplete(autocomp))
+# # #
+# print(forwardGeocode(autocomp))
 
 # def reverseGeocode(coordinates):
 # probably won't need this thing I don't think
--- static/map.js
+++ static/map.js
@@ -1,21 +1,35 @@
- 
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+
+
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+
 function sendData() {
-            var value = document.getElementById('input').value;
-            $.ajax({
+  var addy1 = document.getElementById('addy1').value;
+  var addy2 = document.getElementById('addy2').value;
+  console.log("sending post request")
+  $.ajax({
                 url: '/form',
                 type: 'POST',
-                data: { 'data': value },
+                data: { 'addy1': addy1,
+                        'addy2': addy2},
                 success: function(response) {
-                    var map = L.map('map').setView([response.lo, response.la], 13);
-              L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                  maxZoom: 19,
-                  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-              }).addTo(map);
+                  L.marker(response[0]).addTo(map)
+                  L.marker(response[1]).addTo(map)
+                  console.log(response[2])
+                  var polyline= L.polyline(response[2], {color: 'red'}).addTo(map);
+                  map.fitBounds(polyline.getBounds());
+                  // map.fitBounds([response[0], response[1]])
 
                 },
                 error: function(error) {
                     console.log(error);
                 }
             });
-        }
 
+}
